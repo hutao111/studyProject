5cdc47dadb4f756f141a5bbe16d48259c884f38d
hutao <hutao@fulan.com.cn>
2019-06-16 15:36:30 +0800

    整合了 jest   作为es的基础包

---
+++ project-service/src/test/java/org/hutao/projectservice/application/ES/es.java
@@ -0,0 +1,57 @@
+package org.hutao.projectservice.application.ES;
+
+import io.searchbox.client.JestClient;
+import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+import org.hutao.projectapi.domain.DocumentClassify;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class es {
+
+    @Autowired
+    JestClient jestClient;
+
+    @Test
+    public void test(){
+        DocumentClassify documentClassify = new DocumentClassify();
+        documentClassify.setName("taoge");
+        documentClassify.setId(1l);
+        documentClassify.setCode(1);
+        Index build = new Index.Builder(documentClassify).index("study").type("classify").build();
+        try {
+            jestClient.execute(build);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test2(){
+
+        String json = "{\n" +
+                "    \"query\" : {\n" +
+                "        \"match\" : {\n" +
+                "            \"id\" : \"1\"\n" +
+                "        }\n" +
+                "    }\n" +
+                "}";
+        Search builder = new Search.Builder(json).addIndex("study").addType("classify").build();
+
+
+        try {
+            SearchResult result = jestClient.execute(builder);
+            System.out.println(result.getJsonString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
