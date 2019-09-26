a8e13390c4e8c1fbed7713233d16de559c5e7097
hutao <hutao@fulan.com.cn>
2019-09-26 20:45:56 +0800

    Default Changelist

--- project-service/src/main/java/org/hutao/projectservice/application/config/WebConfig.java
+++ project-service/src/main/java/org/hutao/projectservice/application/config/WebConfig.java
@@ -1,7 +1,12 @@
 package org.hutao.projectservice.application.config;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ResourceUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -17,5 +22,21 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		registry.addResourceHandler("/static/**")
 		.addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
 		super.addResourceHandlers(registry);
+	}
+
+
+
+	// 只要网关配置就好 ， 底下的服务不需要配置。两个同时配置还导致了 跨域失败 适得其反。
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true); // 允许cookies跨域
+		config.addAllowedOrigin("*");// #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
+		config.addAllowedHeader("*");// #允许访问的头信息,*表示全部
+		config.setMaxAge(18000L);// 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
+		config.addAllowedMethod("*");// 允许提交请求的方法，*表示全部允许
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 }
